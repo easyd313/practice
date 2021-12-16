3888a959c720132a00a3ce8ad41a4a22462a2819
Dylan Crompton <easyd313@gmail.com>
2021-12-16 15:11:38 -0500

    testing done with positives and negatives.

--- testSelectionsort.java
+++ testSelectionsort.java
@@ -3,6 +3,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class testSelectionSort {
+  private SelectionSort temp;
 	@Test
 	public void test() {
 		testPositive();
@@ -29,12 +30,29 @@ public class testSelectionSort {
         Sortedarr[3] = 9;
         Sortedarr[4] = 10;
 
-        /** add tests to check for this unit test **/
+        SelectionSort temp = new SelectionSort();
+
+        assertArrayEquals(Sortedarr, temp.basicSelectionSort(arr));
     }
 
     public void testNegative(){
+    	int[] arr = new int[5];
+        arr[0] = -8;
+        arr[1] = -9;
+        arr[2] = -7;
+        arr[3] = -10;
+        arr[4] = -2;
 
-        /** Test data contains negative values only **/
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = -10;
+        Sortedarr[1] = -9;
+        Sortedarr[2] = -8;
+        Sortedarr[3] = -7;
+        Sortedarr[4] = -2;
+
+        SelectionSort temp = new SelectionSort();
+
+        assertArrayEquals(Sortedarr, temp.basicSelectionSort(arr));
     }
 
     public void testMixed(){
