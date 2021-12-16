3662dc3c2bf6030c82958aac6b8ce64501a8d542
Dylan Crompton <easyd313@gmail.com>
2021-12-16 15:12:54 -0500

    testing done with mixed and duplicates

--- testSelectionsort.java
+++ testSelectionsort.java
@@ -3,6 +3,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class testSelectionSort {
+  private SelectionSort temp;
 	@Test
 	public void test() {
 		testPositive();
@@ -38,13 +39,43 @@ public class testSelectionSort {
     }
 
     public void testMixed(){
+    	int[] arr = new int[5];
+        arr[0] = 8;
+        arr[1] = -9;
+        arr[2] = 7;
+        arr[3] = -10;
+        arr[4] = 2;
 
-        /** Test data contains with both positive, negative and zeros **/
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = -10;
+        Sortedarr[1] = -9;
+        Sortedarr[2] = 2;
+        Sortedarr[3] = 7;
+        Sortedarr[4] = 8;
+
+        SelectionSort temp = new SelectionSort();
+
+        assertArrayEquals(Sortedarr, temp.basicSelectionSort(arr));
     }
 
     public void testDuplicates(){
+    	int[] arr = new int[5];
+        arr[0] = 8;
+        arr[1] = 9;
+        arr[2] = 8;
+        arr[3] = 10;
+        arr[4] = 2;
 
-        /** Test data contains duplicates **/
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = 2;
+        Sortedarr[1] = 8;
+        Sortedarr[2] = 8;
+        Sortedarr[3] = 9;
+        Sortedarr[4] = 10;
+
+        SelectionSort temp = new SelectionSort();
+
+        assertArrayEquals(Sortedarr, temp.basicSelectionSort(arr));
     }
 
 
